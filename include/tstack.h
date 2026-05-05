245719df85dbec4e8f4d187d42937abf7845c36c
Tomikota <savinov.ze@yandex.ru>
2026-05-05 20:30:39 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,32 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T data[size];
+    int topIndex;
+public:
+    TStack() : topIndex(-1) {}
+
+    void push(const T& value) {
+        if (topIndex < size - 1)
+            data[++topIndex] = value;
+    }
+
+    T pop() {
+        if (topIndex >= 0)
+            return data[topIndex--];
+        return T();
+    }
+
+    T top() const {
+        if (topIndex >= 0)
+            return data[topIndex];
+        return T();
+    }
+
+    bool isEmpty() const {
+        return topIndex == -1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
